6ef36af17257024f2d25f88f2cc16321f0e264d3
KaioAntonio <kaioaar@gmail.com>
2022-10-20 17:03:42 -0300

    inicializando o projeto

---
+++ src/main/java/com/vemser/geekers/GeekersApplication.java
@@ -0,0 +1,13 @@
+package com.vemser.geekers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vemser/geekers/GeekersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vemser.geekers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
