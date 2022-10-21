92b8d9f5d26e024f1291d942ff47edae842cd11a
Felipe-Noguez <felipenoguez@outlook.com.br>
2022-10-21 10:37:33 -0300

    Adicionada Classes e configurações iniciais de Comentarios

---
+++ src/main/java/com/vemser/geekers/entity/Comentario.java
@@ -0,0 +1,40 @@
+package com.vemser.geekers.entity;
+
+public class Comentario {
+
+    private Integer idComentario;
+    private String comentario;
+//    private Usuario usuario;
+
+    public Integer getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(Integer idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+//    public Usuario getUsuario() {
+//        return usuario;
+//    }
+//
+//    public void setUsuario(Usuario usuario) {
+//        this.usuario = usuario;
+//    }
+
+    @Override
+    public String toString() {
+        return "ID " + idComentario +
+//                "\nUsuario: " + usuario.getNome() +
+                " Comentario: " + comentario;
+    }
+
+}
---
+++ src/main/java/com/vemser/geekers/repository/ComentarioRepository.java
@@ -0,0 +1,7 @@
+package com.vemser.geekers.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ComentarioRepository {
+}
